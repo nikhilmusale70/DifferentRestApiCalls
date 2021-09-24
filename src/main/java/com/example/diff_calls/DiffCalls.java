bc53b0c00417b57a88a13c9eddf0781201edb219
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-24 23:19:48 +0530

    [Nikhil] - added name as path variable and printed greeting msg

--- src/main/java/com/example/diff_calls/DiffCalls.java
+++ src/main/java/com/example/diff_calls/DiffCalls.java
@@ -1,8 +1,5 @@
 package com.example.diff_calls;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiffCalls {
-	@RequestMapping("/home")
-	public String diffCalls(@RequestParam("name") String name, HttpSession session) {
-		return "Hello "+name;
+	@RequestMapping("/home/{name}")
+	public String diffCalls(@PathVariable String name) {
+		return "Hi "+name + ", Hello From Bridgelabz";
 	}
 }
