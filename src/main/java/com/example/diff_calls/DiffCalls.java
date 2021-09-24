9db88c7666feeacb87ed6d72618a4f285659bd13
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-25 00:03:06 +0530

    [Nikhil] - added post method and display s hello msg

--- src/main/java/com/example/diff_calls/DiffCalls.java
+++ src/main/java/com/example/diff_calls/DiffCalls.java
@@ -1,14 +1,27 @@
 package com.example.diff_calls;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
 public class DiffCalls {
-	@RequestMapping("/home/{name}")
-	public String diffCalls(@PathVariable String name) {
-		return "Hi "+name + ", Hello From Bridgelabz";
+	
+	@RequestMapping(value = "/home" , method = RequestMethod.POST)
+	@ResponseBody
+	public String diffCalls(@RequestBody Map<String, String>json) {
+		System.out.println("hi");
+		return "Hi "+json.get("fname")+" "+json.get("lname") + ", Hello From Bridgelabz";
+
 	}
 }
---
+++ src/main/java/com/example/diff_calls/Man.java
@@ -0,0 +1,22 @@
+package com.example.diff_calls;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Man {
+	String fname;
+	String lname;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+}
