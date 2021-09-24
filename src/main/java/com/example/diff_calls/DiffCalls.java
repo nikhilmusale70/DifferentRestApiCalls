f672f20452c820f20c609ca0c9f391504f085920
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-24 22:53:59 +0530

    [Nikhil] - taking user's name from url and printing a greeting msg

--- src/main/java/com/example/diff_calls/DiffCalls.java
+++ src/main/java/com/example/diff_calls/DiffCalls.java
@@ -1,12 +1,16 @@
 package com.example.diff_calls;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiffCalls {
 	@RequestMapping("/home")
-	public String diffCalls() {
-		return "Hello World, With RESTfull apis";
+	public String diffCalls(HttpServletRequest req) {
+		String name = req.getParameter("name");
+		return "Hello "+name;
 	}
 }
