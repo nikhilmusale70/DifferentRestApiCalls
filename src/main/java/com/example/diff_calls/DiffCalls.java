028df5ed66cbd86f3cda5cd61be1722a80242ad8
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-24 23:02:02 +0530

    [Nikhil] - taking user's name from url and printing a greeting msg through spring boot

--- src/main/java/com/example/diff_calls/DiffCalls.java
+++ src/main/java/com/example/diff_calls/DiffCalls.java
@@ -1,16 +1,17 @@
 package com.example.diff_calls;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiffCalls {
 	@RequestMapping("/home")
-	public String diffCalls(HttpServletRequest req) {
-		String name = req.getParameter("name");
+	public String diffCalls(@RequestParam("name") String name, HttpSession session) {
 		return "Hello "+name;
 	}
 }
