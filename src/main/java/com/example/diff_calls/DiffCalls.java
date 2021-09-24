9b4f553e9e961f020603614a6b1a503951cab869
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-25 00:21:41 +0530

    [Nikhil] - added RequestParam, PathVariable and printed a message

--- src/main/java/com/example/diff_calls/DiffCalls.java
+++ src/main/java/com/example/diff_calls/DiffCalls.java
@@ -1,27 +1,20 @@
 package com.example.diff_calls;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 public class DiffCalls {
 	
-	@RequestMapping(value = "/home" , method = RequestMethod.POST)
-	@ResponseBody
-	public String diffCalls(@RequestBody Map<String, String>json) {
+	@RequestMapping(value = "/home/{fname}")
+	public String diffCalls(@PathVariable("fname") String name,@RequestParam("lname") String lname) {
 		System.out.println("hi");
-		return "Hi "+json.get("fname")+" "+json.get("lname") + ", Hello From Bridgelabz";
+		return "Hi "+name+" " +lname + ", Hello From Bridgelabz";
 
 	}
+	
 }
